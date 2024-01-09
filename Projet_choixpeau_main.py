29853b6a0efcbb06df0fcbd51d216a5e9d9ae2a8
Nodd20 <146093790+Nodd20@users.noreply.github.com>
2024-01-09 14:14:19 +0100

    Update Projet_choixpeau_main.py

--- Projet_choixpeau_main.py
+++ Projet_choixpeau_main.py
@@ -1,1 +1,12 @@
 # coding: utf-8
+'''
+        ...
+'''
+
+def k_ppv_algo(profil_cible, donnees_profil, k=5):
+    '''
+    '''
+    assert profil_cible == list; donnees_profil == list; k == int
+    pass
+
+    
