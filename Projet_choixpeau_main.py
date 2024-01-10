ef186f019541f154f713cac86cd9d5a678b6bb1c
Nodd20 <146093790+Nodd20@users.noreply.github.com>
2024-01-10 17:02:16 +0100

    Update Projet_choixpeau_main.py

--- Projet_choixpeau_main.py
+++ Projet_choixpeau_main.py
@@ -11,10 +11,14 @@ def k_ppv_algo(profile, profile_data, k=5):
     '''
     '''
     assert profile == dict; profile_data == list; k == int
-    distance_list = []
+    distance_dict = {}
     for i in range(len(profile_data)):
         for compared in profile_data:
                 distance = ((compared['Courage'] - profile['Courage']) ** 2 + (compared['Ambition'] - profile['Ambition']) ** 2 + (compared['Intelligence'] - profile['Intelligence']) ** 2 + (compared['Good'] - profile['Good']) ** 2) ** 1/2
-                distance_list.append(distance)
+                distance_dict.update(compared['Name'], distance)
+
+    distance_dict['Name'].sort()
+    for j in range(k + 1):
+        distance_dict[k] 
 
     
