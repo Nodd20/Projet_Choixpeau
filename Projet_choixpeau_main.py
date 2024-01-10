4b7752c7c43695ad14e493f1182e9e8e81391a67
Nodd20 <146093790+Nodd20@users.noreply.github.com>
2024-01-10 18:10:46 +0100

    H

--- Projet_choixpeau_main.py
+++ Projet_choixpeau_main.py
@@ -11,14 +11,13 @@ def k_ppv_algo(profile, profile_data, k=5):
     '''
     '''
     assert profile == dict; profile_data == list; k == int
-    distance_dict = {}
+    distance_tab = []
     for i in range(len(profile_data)):
         for compared in profile_data:
-                distance = ((compared['Courage'] - profile['Courage']) ** 2 + (compared['Ambition'] - profile['Ambition']) ** 2 + (compared['Intelligence'] - profile['Intelligence']) ** 2 + (compared['Good'] - profile['Good']) ** 2) ** 1/2
-                distance_dict.update(compared['Name'], distance)
+            distance_dict = {}
+            distance = ((compared['Courage'] - profile['Courage']) ** 2 + (compared['Ambition'] - profile['Ambition']) ** 2 + (compared['Intelligence'] - profile['Intelligence']) ** 2 + (compared['Good'] - profile['Good']) ** 2) ** 1/2
+            distance_dict.update[distance, compared['Name']]
 
-    distance_dict['Name'].sort()
-    for j in range(k + 1):
-        distance_dict[k] 
-
-    
+    distance_tab.sort()
+    for j in range(k):
+        distance_tab[k] 
--- import csv.py
+++ import csv.py
@@ -1,10 +1,10 @@
 import csv
 
-with open("Caracteristiques_des_persos", mode='r', encoding='utf-8') as f:
+with open("Caracteristiques_des_persos.csv", mode='r', encoding='utf-8') as f:
     reader = csv.DictReader(f, delimiter=';')
     a = [{key : value for key, value in element.items()} for element in reader]
 
-with open("Characters", mode='r', encoding='utf-8') as f:
+with open("Characters.csv", mode='r', encoding='utf-8') as f:
     reader = csv.DictReader(f, delimiter=';')
     ab = [{key : value for key, value in element.items()} for element in reader]
 
