2e1e78f9083c7df2a5c5d2465d47bd95b108a524
LeMoutonRouge <151654996+LeMoutonRouge@users.noreply.github.com>
2024-01-15 15:32:46 +0100

    Add files via upload

---
+++ fusion de table yes.py
@@ -0,0 +1,12 @@
+import csv
+
+with open("Caracteristiques_des_persos.csv", mode='r', encoding='utf-8') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    caracteristiques_persos = [{key : value for key, value in element.items()} for element in reader]
+
+with open("Characters.csv", mode='r', encoding='utf-8') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    caracteristiques_persos_tout = [{key : value for key, value in element.items()} for element in reader]
+
+print(caracteristiques_persos_tout)
+
