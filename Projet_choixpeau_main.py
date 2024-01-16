ab666ab7b483223a33f5c1021a5bdf50248c4544
Nodd20 <146093790+Nodd20@users.noreply.github.com>
2024-01-16 14:52:04 +0100

    Update Projet_choixpeau_main.py

--- Projet_choixpeau_main.py
+++ Projet_choixpeau_main.py
@@ -3,18 +3,32 @@
         ...
 '''
 
+import csv
+
+with open("Caracteristiques_des_persos.csv", mode='r', encoding='utf-8') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    caracteristiques_persos = [{key : value for key, value in element.items()} for element in reader]
+
+with open("Characters.csv", mode='r', encoding='utf-8') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    caracteristiques_persos_tout = [{key : value for key, value in element.items()} for element in reader]
+
+
 
 def k_ppv_algo(profile, profile_data, k=5):
     '''
     '''
-    assert profile == dict; profile_data == list; k == int
     distance_tab = []
     for i in range(len(profile_data)):
         for compared in profile_data:
-            distance_dict = {}
-            distance = ((compared['Courage'] - profile['Courage']) ** 2 + (compared['Ambition'] - profile['Ambition']) ** 2 + (compared['Intelligence'] - profile['Intelligence']) ** 2 + (compared['Good'] - profile['Good']) ** 2) ** 1/2
-            distance_dict.update[distance, compared['Name']]
+            distance = ((int(compared['Courage']) - profile['Courage']) ** 2 + (int(compared['Ambition']) - profile['Ambition']) ** 2 + (int(compared['Intelligence']) - profile['Intelligence']) ** 2 + (int(compared['Good']) - profile['Good']) ** 2) ** 1/2
+            distance_tab.append({compared['Name'], distance})
+            print(distance)
 
-    distance_tab.sort()
     for j in range(k):
-        distance_tab[k] 
+        print(distance_tab[k])
+
+
+aimed_profile = {'Courage': 9, 'Ambition': 2, 'Intelligence': 8, 'Good': 9}
+
+k_ppv_algo(aimed_profile, caracteristiques_persos, 5)
