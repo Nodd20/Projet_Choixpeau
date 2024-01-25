db52492868af2b98ca83bd3ed7a8fdaa36a2d8b0
Nodd20 <146093790+Nodd20@users.noreply.github.com>
2024-01-25 16:13:59 +0100

    Update Projet_choixpeau_main.py

--- Projet_choixpeau_main.py
+++ Projet_choixpeau_main.py
@@ -5,24 +5,10 @@
 
 import csv
 
-with open("Caracteristiques_des_persos.csv", mode='r', encoding='utf-8') as f:
-    reader = csv.DictReader(f, delimiter=';')
-    caracteristiques_persos = [{key : value for key, value in element.items()} for element in reader]
-
-with open("Characters.csv", mode='r', encoding='utf-8') as f:
-    reader = csv.DictReader(f, delimiter=';')
-    caracteristiques_persos_tout = [{key : value for key, value in element.items()} for element in reader]
-
-list_characters = []
-
-for persos_1 in caracteristiques_persos:
-    for persos_2 in caracteristiques_persos_tout:
-        if persos_1['Name'] == persos_2['Name']:
-            persos_1.update(persos_2)
-            list_characters.append(persos_1)
 
 def k_ppv_algo(profile, profile_data, k=5):
     '''
+    
     '''
     distance_tab = []
     for compared in profile_data:
@@ -50,7 +36,6 @@ def k_ppv_algo(profile, profile_data, k=5):
         
     liste_choixpeau = [gryffondor, serdaigle, serpentard, poufsouffle]
     liste_choixpeau.sort(reverse=True)
-    print(liste_choixpeau)
 
     if liste_choixpeau[0] == liste_choixpeau[1]:
         choixpeau = k_ppv_algo(profile, profile_data, 3)
@@ -66,9 +51,21 @@ def k_ppv_algo(profile, profile_data, k=5):
     return choixpeau, tab_voisins
 
 
-aimed_profile = {'Courage': 5, 'Ambition': 5, 'Intelligence': 5, 'Good': 5}
+with open("Caracteristiques_des_persos.csv", mode='r', encoding='utf-8') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    caracteristiques_persos = [{key : value for key, value in element.items()} for element in reader]
 
-print(k_ppv_algo(aimed_profile, list_characters))
+with open("Characters.csv", mode='r', encoding='utf-8') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    caracteristiques_persos_tout = [{key : value for key, value in element.items()} for element in reader]
+
+list_characters = []
+
+for persos_1 in caracteristiques_persos:
+    for persos_2 in caracteristiques_persos_tout:
+        if persos_1['Name'] == persos_2['Name']:
+            persos_1.update(persos_2)
+            list_characters.append(persos_1)
 
 profile_1 = {'Courage': 9, 'Ambition': 2, 'Intelligence': 8, 'Good': 9}
 profile_2 = {'Courage': 6, 'Ambition': 7, 'Intelligence': 9, 'Good': 7}
@@ -76,9 +73,26 @@ profile_3 = {'Courage': 3, 'Ambition': 8, 'Intelligence': 6, 'Good': 3}
 profile_4 = {'Courage': 2, 'Ambition': 3, 'Intelligence': 7, 'Good': 8}
 profile_5 = {'Courage': 3, 'Ambition': 4, 'Intelligence': 8, 'Good': 8}
 
+print(f"La maison du profil 1 est {k_ppv_algo(profile_1, list_characters)[0]}, car vos voisins sont {k_ppv_algo(profile_1, list_characters)[1]}")
+print(f"La maison du profil 2 est {k_ppv_algo(profile_2, list_characters)[0]}, car vos voisins sont {k_ppv_algo(profile_2, list_characters)[1]}")
+print(f"La maison du profil 3 est {k_ppv_algo(profile_3, list_characters)[0]}, car vos voisins sont {k_ppv_algo(profile_3, list_characters)[1]}")
+print(f"La maison du profil 4 est {k_ppv_algo(profile_4, list_characters)[0]}, car vos voisins sont {k_ppv_algo(profile_4, list_characters)[1]}")
+print(f"La maison du profil 5 est {(k_ppv_algo(profile_5, list_characters))[0][0]}, car vos voisins sont {k_ppv_algo(profile_5, list_characters)[1]}")
 
-print(f"La maison du profil 1 est {k_ppv_algo(profile_1, list_characters, 5)[0]}, car vos voisins sont {k_ppv_algo(profile_1, list_characters, 5)[1]}")
-print(f"La maison du profil 2 est {k_ppv_algo(profile_2, list_characters, 5)[0]}, car vos voisins sont {k_ppv_algo(profile_2, list_characters, 5)[1]}")
-print(f"La maison du profil 3 est {k_ppv_algo(profile_3, list_characters, 5)[0]}, car vos voisins sont {k_ppv_algo(profile_3, list_characters, 5)[1]}")
-print(f"La maison du profil 4 est {k_ppv_algo(profile_4, list_characters, 5)[0]}, car vos voisins sont {k_ppv_algo(profile_4, list_characters, 5)[1]}")
-print(f"La maison du profil 5 est {k_ppv_algo(profile_5, list_characters, 5)[0]}, car vos voisins sont {k_ppv_algo(profile_5, list_characters, 5)[1]}")
+answer = int(input("Saisissez 1 si vous voulez afficher les profils préséléctionnés. Saisissez 2 pour entrer un profil."))
+
+while answer == 1 or answer == 2:
+    if answer == 1:
+        pass 
+
+    else:
+        courage = int(input("De 1 à 9 quel est son courage ?"))
+        ambition = int(input("De 1 à 9 quel est son ambition ?"))
+        intelligence = int(input("De 1 à 9 quel est son intelligence"))
+        good = int(input("De 1 à 9 quel est sa bonté ?"))
+
+        profile_settings = {'Courage': courage, 'Ambition': ambition, 'Intelligence': intelligence, 'Good': good}
+
+        house = k_ppv_algo(profile_settings, list_characters)[0][0]
+        print(f"Votre personnage est de la maison :{house}, ses voisins sont{k_ppv_algo(profile_settings, list_characters)}")
+        answer = int(input("Saisissez 1 si vous voulez afficher les profils préséléctionnés. Saisissez 2 pour entrer un profil."))
