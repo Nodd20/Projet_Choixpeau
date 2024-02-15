1a4fc6b293d10ce459db4b8ce6a7b31728933ce5
Nodd20 <146093790+Nodd20@users.noreply.github.com>
2024-02-15 16:41:47 +0100

    grosse_updt

--- partie 2 choipeau magique.py
+++ partie 2 choipeau magique.py
@@ -12,3 +12,35 @@ Licence : CC-BY-NC-SA
 github : https://github.com/Nodd20/Projet_Choixpeau 
 '''
 
+from csv import *
+
+def importeur(fichier):
+    '''
+    '''
+    assert fichier == str
+
+    with open(fichier, mode='r', encoding='utf-8') as f:
+        reader = csv.DictReader(f, delimiter=';')
+        element = [{key : value for key, value in element.items()} for element in reader]
+
+    return element
+    
+
+def questionnaire(question_num, table_question):
+    '''
+    '''
+    assert question_num == int
+    assert table_question == list
+
+    question = table_question[question_num]['Question']
+    return question
+
+def caracteriseur(dico_cara, reponse, question_num, table_question):
+    '''
+    '''
+    assert reponse == int 
+    assert dico_cara == dict
+
+    categorie = table_question[question_num]['Categorie']
+    dico_cara[categorie] = dico_cara[categorie] + reponse
+    
